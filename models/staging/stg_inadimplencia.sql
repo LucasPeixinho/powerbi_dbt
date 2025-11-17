796ca2145702ed02c16ce0ac415251e121bbcb98
Lucas Marques <lucasmarques@cdp.lub>
2025-11-17 09:06:59 -0300

    fix: update query period in stg_inadimplencia

--- models/staging/stg_inadimplencia.sql
+++ models/staging/stg_inadimplencia.sql
@@ -13,5 +13,5 @@ SELECT
 FROM
     cedep.PCPREST
 WHERE
-    DTVENC BETWEEN TO_DATE('{{ var("dt_inicio", "2020-01-01") }}', 'YYYY-MM-DD')
+    DTVENC BETWEEN TO_DATE('{{ var("dt_inicio", "2018-01-01") }}', 'YYYY-MM-DD')
                 AND TO_DATE('{{ var("dt_fim", "2025-12-31") }}', 'YYYY-MM-DD')
